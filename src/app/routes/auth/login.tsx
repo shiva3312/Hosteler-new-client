d93fc9530cf0b909a523388f9842d7790b1f03e8
shivam chaurasiya <shivam.chaurasiya@bentley.com>
2025-07-16 00:31:24 +0530

    cache issue fixed

--- src/app/routes/auth/login.tsx
+++ src/app/routes/auth/login.tsx
@@ -4,12 +4,13 @@ import { useNavigate, useSearchParams } from 'react-router';
 
 import { LoginForm } from '@/components/ui/auth/login-form';
 import { paths } from '@/config/paths';
+import { useMe } from '@/lib/api/user/get-me';
 
 const LoginRoute = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get('redirectTo');
-
+  const { refetch } = useMe();
   return (
     <Box w={400} mx="auto" mt={100} p={20}>
       <Center my={'md'}>
@@ -20,6 +21,7 @@ const LoginRoute = () => {
 
       <LoginForm
         onSuccess={() => {
+          refetch();
           navigate(
             `${redirectTo ? `${redirectTo}` : paths.app.dashboard.getHref()}`,
             {
